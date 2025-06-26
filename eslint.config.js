5ea72987f89852458a5b504b3618bc4400497892
JustSouichi <tommasobertocchideveloper04@gmail.com>
2025-06-26 14:32:32 +0200

    *

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,30 @@
 import js from '@eslint/js';
+import * as importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
-  js.configs.recommended,
+  js.configs.recommended,         // base ESLint rules
+  importPlugin.configs.recommended,
   {
+    plugins: {
+      import: importPlugin,
+      prettier,
+    },
+    rules: {
+      // tue regole personalizzate
+      'prettier/prettier': 'error',
+      'import/no-unresolved': 'error',
+      // ...
+    },
     languageOptions: {
-      ecmaVersion: 'latest',      // let espree handle modern syntax
+      ecmaVersion: 'latest',
       sourceType: 'module',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    rules: {
+      // override per i test
     },
   },
 ];
